57969488b697d155682fdc05b0d2373fdad29a1a
indusRealEstate <webtech@indusre.ae>
2024-02-06 12:27:39 +0400

    fix: :bug: fixed next js version issue.

--- next.config.js
+++ next.config.js
@@ -1,17 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    loader: "default",
-    disableStaticImages: true,
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "premium.indusre.com",
-      },
-    ],
-    // deviceSizes: [320, 420, 768, 1024, 1200],
+    domains: ["premium.indusre.com"],
     minimumCacheTTL: 0,
-    // unoptimized: true,
   },
 };
 
